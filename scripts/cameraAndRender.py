d375a3c65f36cfd436d0bc07d6d1af371ed4d0c5
jbishop-art <jonathan.bishop.art@gmail.com>
2019-02-12 14:04:35 -0700

    Initial Script structure set up
    Initial Script structure set up

---
+++ scripts/cameraAndRender.py
@@ -0,0 +1,13 @@
+#Import maya.cmds and abrevaite them to "mc".
+import maya.cmds as cmds
+#Import math
+from math import pow,sqrt
+
+ 
+'''
+*********************************************************************
+RUN DEFINITIONS
+*********************************************************************
+''' 
+       
+#run defs    
---
+++ scripts/createPolyCube.py
@@ -0,0 +1,18 @@
+#Import maya.cmds and abrevaite them to "mc".
+import maya.cmds as cmds
+#Import math
+from math import pow,sqrt
+
+#create poly cube at origin and rename it cube
+def cube():
+    cmds.polyCube()
+    cmds.rename('pCube1', 'polyCube')
+ 
+'''
+*********************************************************************
+RUN DEFINITIONS
+*********************************************************************
+''' 
+       
+#run defs    
+cube()
